ba0266e588cff2b357ae57c4a79d38c93cb455a6
sas <mishra.shashank9598@gmail.com>
2021-07-17 14:49:49 +0530

    Solution finished

---
+++ Codes/solution.py
@@ -0,0 +1,43 @@
+def solve(nums,lower,upper):
+    results = []  
+    #if input arrar is empty      
+    if not nums:
+        results.append(pat_gen(lower, upper))
+        return results
+
+    #range is inclusive so using prev_num to check the first item
+    prev_num = lower - 1
+
+    for num in nums:
+        if prev_num != num and num - 1 != prev_num:                
+            missing = pat_gen(prev_num + 1, num - 1)                
+            results.append(missing)
+            prev_num = num                
+        else:
+            prev_num = num
+            
+    if nums[-1] != upper:
+        missing = pat_gen(nums[-1] + 1, upper)
+        results.append(missing)    
+    
+    return results
+
+#format the range in string as per the requried output format
+def pat_gen(left, right):
+    return str(left) if left == right else str(left) + "->" + str(right)
+
+
+
+# #Test Cases:
+
+# #Test Case 1: Sample
+# #nums=[0, 1, 3, 50, 75]
+# print(solve(nums, 0, 99))
+
+# #Test Case 2: Empty Array
+# nums=[]
+# print(solve(nums, 0, 99))
+
+#Test Case 3: negative values
+# nums=[-12, 0, 1, 3, 50, 75]
+# print(solve(nums, -12, 100))
